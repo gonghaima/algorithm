897df518f659f3df941a7b4f0b3a2d11ab59c2ed
Steven <gonghaima@hotmail.com>
2018-07-14 18:14:08 +1000

    Bubble sort added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import {BinarySearch} from './BinarySearch';
-
+/** Binary search */
+import { BinarySearch } from './binarySearch';
 const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-
 const BinarySearchResult = BinarySearch(data, 8);
+console.log(`BinarySearchResult - index of the given value: ${BinarySearchResult}`);
 
-console.log(BinarySearchResult);
+
+
+
+
+/** Bubble sort */
+const dataBubleSort = [1, 8, 2, 9, 3, 4, 6, 7, 5, 10];
+import { BubbleSort } from './bableSort';
+const BableSortResult = BubbleSort(dataBubleSort);
+console.log(`BableSortResult: ${BableSortResult}`);
+
+
