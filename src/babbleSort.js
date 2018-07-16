cf4e3f8545bc8abe192e1b9747b03c6596e1fd40
Steven <gonghaima@hotmail.com>
2018-07-16 20:29:59 +1000

    another babble sort

--- src/babbleSort.js
+++ src/babbleSort.js
@@ -1,15 +1,29 @@
+// export const BubbleSort = (a) => {
+//   let swapped;
+//   do {
+//     swapped = false;
+//     for (var i = 0; i < a.length - 1; i++) {
+//       if (a[i] > a[i + 1]) {
+//         var temp = a[i];
+//         a[i] = a[i + 1];
+//         a[i + 1] = temp;
+//         swapped = true;
+//       }
+//     }
+//   } while (swapped);
+//   return a;
+// }
+
 export const BubbleSort = (a) => {
-  let swapped;
-  do {
-    swapped = false;
-    for (var i = 0; i < a.length - 1; i++) {
-      if (a[i] > a[i + 1]) {
-        var temp = a[i];
-        a[i] = a[i + 1];
-        a[i + 1] = temp;
-        swapped = true;
+  let arrData = [...a];
+  for (let i = arrData.length; i > 0; i--) {
+    for (let j = 1; j < i; j++) {
+      if (arrData[j - 1] > arrData[j]) {
+        const temp = arrData[j - 1];
+        arrData[j - 1] = arrData[j];
+        arrData[j] = temp;
       }
     }
-  } while (swapped);
-  return a;
+  }
+  return arrData;
 }
