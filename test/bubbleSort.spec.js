bc46b2c9e127e5bf0b187c83fd24acb1769e6976
Steven <gonghaima@hotmail.com>
2018-08-08 17:10:05 +1000

    unit test updated, insert sort is yet completed

--- test/bubbleSort.spec.js
+++ test/bubbleSort.spec.js
@@ -1,9 +1,9 @@
 import test from 'ava';
-import { InsertionSort } from '../src/Sorting/insertionSort';
+import { BubbleSort } from '../src/Sorting/babbleSort';
 
 test('binary search rule should return the position of a given item', t => {
   const data = [2, 3, 4, 6, 7, 9, 10, 5, 1, 8];
   const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const actual = InsertionSort(data);
+  const actual = BubbleSort(data);
   t.deepEqual(actual, expectedResult);
 });
--- test/insertionSort.spec.js
+++ test/insertionSort.spec.js
@@ -1,9 +1,9 @@
 import test from 'ava';
-import { BubbleSort } from '../src/Sorting/babbleSort';
+import { InsertionSort } from '../src/Sorting/insertionSort';
 
-test('binary search rule should return the position of a given item', t => {
+test('InsertionSort should sort an array', t => {
   const data = [2, 3, 4, 6, 7, 9, 10, 5, 1, 8];
   const expectedResult = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const actual = BubbleSort(data);
+  const actual = InsertionSort(data);
   t.deepEqual(actual, expectedResult);
 });
